29ceb4040f571f211271a01dba9aa20220ad97bf
Prabin Singh <pravin544@gmail.com>
2020-07-16 18:37:13 -0500

    changed file structure

--- Develop/script.js
+++ Develop/script.js
@@ -4,7 +4,11 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
+  passwordText.value = password;
+
+  }
   function generatePassword(){
     var passwordLength = prompt("How Long do you need your Password");
     var specialChar = confirm("Click ok to use special Charcter in your password?");
@@ -15,42 +19,38 @@ function writePassword() {
     generatedPassword = "";
     var specialCharacter = ["!", "@", "#", "$", "%", "^", "&", "*"]
     count = 0;
-
-    while (count <= passwordLength){
-        for (let i = 0; i < passwordLength; i++) {
-            if (numericValue) {
-                var randNum = Math.floor(Math.random() * 10);
-                generatedPassword = generatedPassword + randNum;
-                count++;
-            }
-            if (upperCase){
-                var randNum = Math.floor(Math.random() * 26);
-                var randChar = String.fromCharCode(65 + randNum);
-                generatedPassword = generatedPassword + randChar;
-                count++;
-            }
-            if (lowerCase){
-                var randNum = Math.floor(Math.random() * 26);
-                var randChar = String.fromCharCode(65 + randNum);
-                generatedPassword = generatedPassword + randChar.toLowerCase();
-                count++;
-            }
-            if (specialChar){
-                var randNum = Math.floor(Math.random() * 7);
-                var randSpecialChar = specialCharacter[randNum];
-                generatedPassword = generatedPassword + randSpecialChar;
-                count++;
-                
-            }
-            
+    
+    while (count < passwordLength) {
+        if (numericValue) {
+            var randNum = Math.floor(Math.random() * 10);
+            generatedPassword = generatedPassword + randNum;
+            console.log(numericValue);
+            count++;
         }
+        if (upperCase){
+            var randNum = Math.floor(Math.random() * 26);
+            var randChar = String.fromCharCode(65 + randNum);
+            generatedPassword = generatedPassword + randChar;
+            console.log(upperCase);
+            count++;
+        }
+        if (lowerCase){
+            var randNum = Math.floor(Math.random() * 26);
+            var randChar = String.fromCharCode(65 + randNum);
+            generatedPassword = generatedPassword + randChar.toLowerCase();
+            console.log(lowerCase);
+            count++;
+        }
+        if (specialChar){
+            var randNum = Math.floor(Math.random() * 7);
+            var randSpecialChar = specialCharacter[randNum];
+            generatedPassword = generatedPassword + randSpecialChar;
+            console.log(specialChar);
+            count++;
+        }
+          
     }
     return generatedPassword
-  }
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
 }
 
 // Add event listener to generate button
