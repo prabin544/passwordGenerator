7fe7ef0d30cf6c98bab365a5d55f6ed59a50bca7
Prabin Singh <pravin544@gmail.com>
2020-07-16 19:31:46 -0500

    Added condition to check password length

--- Develop/script.js
+++ Develop/script.js
@@ -11,46 +11,48 @@ function writePassword() {
   }
   function generatePassword(){
     var passwordLength = prompt("How Long do you need your Password");
-    var specialChar = confirm("Click ok to use special Charcter in your password?");
-    var upperCase = confirm("Click ok to use Upper case in your password?");
-    var lowerCase = confirm("Click ok to use lower case in your password?");
-    var numericValue = confirm("Click ok to use numeric value in your password?");
-
-    generatedPassword = "";
-    var specialCharacter = ["!", "@", "#", "$", "%", "^", "&", "*"]
-    count = 0;
-    
-    while (count < passwordLength) {
-        if (numericValue) {
-            var randNum = Math.floor(Math.random() * 10);
-            generatedPassword = generatedPassword + randNum;
-            console.log(numericValue);
-            count++;
-        }
-        if (upperCase){
-            var randNum = Math.floor(Math.random() * 26);
-            var randChar = String.fromCharCode(65 + randNum);
-            generatedPassword = generatedPassword + randChar;
-            console.log(upperCase);
-            count++;
-        }
-        if (lowerCase){
-            var randNum = Math.floor(Math.random() * 26);
-            var randChar = String.fromCharCode(65 + randNum);
-            generatedPassword = generatedPassword + randChar.toLowerCase();
-            console.log(lowerCase);
-            count++;
-        }
-        if (specialChar){
-            var randNum = Math.floor(Math.random() * 7);
-            var randSpecialChar = specialCharacter[randNum];
-            generatedPassword = generatedPassword + randSpecialChar;
-            console.log(specialChar);
-            count++;
-        }
-          
+    if (passwordLength < 4){
+        confirm("Passowrd should be atleast 4 character long");
     }
-    return generatedPassword
+    else{
+        var specialChar = confirm("Click ok to use special Charcter in your password?");
+        var upperCase = confirm("Click ok to use Upper case in your password?");
+        var lowerCase = confirm("Click ok to use lower case in your password?");
+        var numericValue = confirm("Click ok to use numeric value in your password?");
+        
+        generatedPassword = "";
+        var specialCharacter = ["!", "@", "#", "$", "%", "^", "&", "*"]
+        count = 0;
+        
+        while (count < passwordLength) {
+            if (numericValue && count < passwordLength) {
+                var randNum = Math.floor(Math.random() * 10);
+                generatedPassword = generatedPassword + randNum;
+                count++;
+            }
+            if (upperCase && count < passwordLength){
+                var randNum = Math.floor(Math.random() * 26);
+                var randChar = String.fromCharCode(65 + randNum);
+                generatedPassword = generatedPassword + randChar;
+                count++;
+            }
+            if (lowerCase && count < passwordLength){
+                var randNum = Math.floor(Math.random() * 26);
+                var randChar = String.fromCharCode(65 + randNum);
+                generatedPassword = generatedPassword + randChar.toLowerCase();
+                count++;
+            }
+            if (specialChar && count < passwordLength){
+                var randNum = Math.floor(Math.random() * 7);
+                var randSpecialChar = specialCharacter[randNum];
+                generatedPassword = generatedPassword + randSpecialChar;
+                count++;
+            }
+            
+        }
+        return generatedPassword
+    }
+    
 }
 
 // Add event listener to generate button
